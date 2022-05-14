b0ec2bb367ac9c0dc13b9d6140d312c705f413de
Busayo Samuel <busayosamuel2016@gmail.com>
2022-05-14 16:37:04 +0100

    cors issue

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ const cors = require('cors')
 const typeDefs = require("./graphql/typedefs");
 const { MONGODB } = require("./config");
 const resolvers = require("./graphql/resolvers");
-
+var corsOptions = {
+  origin: '*',
+  credentials: true
+};
 const PORT = process.env.PORT || PORT;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req }) => ({ req }),
-  cors: false
+  cors: corsOptions
 });
-server.use(cors)
 mongoose
   .connect(MONGODB, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => console.log("mongobd running"), server.listen({ port: PORT }))
